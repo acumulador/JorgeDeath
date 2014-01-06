68d51b6edd8600f2ea83bb0ba84df30599d13b0c
JuanC Salazar <jcsalazarm@gmail.com>
2014-01-06 08:12:42 -0500

    Initial Commit

---
+++ JorgeDeath/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  JorgeDeath
+//
+//  Created by Juan C Salazar on 6/01/14.
+//  Copyright Juan C Salazar 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
